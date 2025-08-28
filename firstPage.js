946b330d4ab6885b88ee4a3062210f04d74d0453
Skoco Pavle <skocopavle@gmail.com>
2025-08-28 20:32:29 +0200

    Here the final version of the first page

--- firstPage.js
+++ firstPage.js
@@ -1,3 +1,4 @@
+// Dugmad za vrstu transporta
 const buttons = document.querySelectorAll('.buttons button');
 const originInput = document.getElementById('origin');
 const destinationInput = document.getElementById('destination');
@@ -10,7 +11,7 @@ buttons.forEach(button => {
         } 
         else if (button.classList.contains("ocean-lcl")) {
             originInput.placeholder = "Origin for Ocean LCL";
-            destinationInput.placeholder = "Destination for Ocean FLC";
+            destinationInput.placeholder = "Destination for Ocean LCL";
         } 
         else if (button.classList.contains("ocean-roro")) {
             originInput.placeholder = "Origin for Ocean RORO";
@@ -22,7 +23,23 @@ buttons.forEach(button => {
         } 
         else if (button.classList.contains("air")) {
             originInput.placeholder = "Origin for Air";
-            destinationInput.placeholder = "Destinaton for air";
+            destinationInput.placeholder = "Destination for Air";
         }
     });
+});
+
+// Popup
+const popup = document.getElementById('popup');
+const submitBtn = document.querySelector('.submit-btn');
+const trackBtn = document.querySelector('.track-button');
+
+// Otvara popup
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    popup.classList.add('show');
+});
+
+// Zatvara popup
+trackBtn.addEventListener('click', () => {
+    popup.classList.remove('show');
 });
